3bee1815ac25ede04c193cacab6f3068dc99624d
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-20 15:55:33 +0300

    lesson-1: add task3

---
+++ Lesson1/Task3/Author.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Author
+    {
+        string _name;
+        public string Name
+        {
+            get
+            {
+                if (_name != null)
+                {
+                    return _name;
+                }
+                else
+                {
+                    return "There is no author!";
+                }
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Author: {Name}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+
+        }
+    }
+}
---
+++ Lesson1/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creat a book");
+            Console.WriteLine("Input: title, author, content");
+            string title = Console.ReadLine();
+            string author = Console.ReadLine();
+            string content = Console.ReadLine();
+            Book book = new Book(title, author, content);
+            book.Show();
+
+            Console.ReadKey();
+        }
+    }
+}
