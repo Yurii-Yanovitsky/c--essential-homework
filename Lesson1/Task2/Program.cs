3e59352c58137dc7a61b842f2721352392cdfac1
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-20 13:54:46 +0300

    lesson-1: add task2

---
+++ Lesson1/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input rectangle sides:");
+            Console.Write("side1: ");
+            double side1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("side2: ");
+            double side2 = Convert.ToDouble(Console.ReadLine());
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine($"Area: {rectangle.Area}");
+            Console.WriteLine($"Perimeter: {rectangle.Perimeter}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson1/Task2/Rectangle.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Rectangle
+    {
+        double _side1;
+        double _side2;
+        public double Area
+        {
+            get
+            {
+                return AreaCalculator();
+            }
+        }
+        public double Perimeter
+        {
+            get
+            {
+                return PerimeterCalculator();
+            }
+        }
+        public Rectangle(double side1, double side2)
+        {
+            _side1 = side1;
+            _side2 = side2;
+        }
+        public Rectangle()
+        {
+
+        }
+        public double AreaCalculator()
+        {
+            return _side1 * _side2;
+        }
+        public double PerimeterCalculator()
+        {
+            return 2 * (_side1 + _side2);
+        }
+    }
+}
