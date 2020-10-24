592d9b9bd549c047144281f2ececc76b9ae0a303
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-24 16:05:39 +0300

    lesson-2: add additional task

---
+++ Lesson2/AdditionalTask/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User("fidelcastro", "Fidel", "Castro", 50);
+            ShowUserInfo(user);
+
+            Console.ReadKey();
+        }
+        static void ShowUserInfo(User user)
+        {
+            Console.WriteLine($"Login: {user.Login}");
+            Console.WriteLine($"Name: {user.Name}");
+            Console.WriteLine($"LastName: {user.LastName}");
+            Console.WriteLine($"Age: {user.Age}");
+            Console.WriteLine($"Date of filling: {user._date}");
+        }
+    }
+
+}
---
+++ Lesson2/AdditionalTask/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    class User
+    {
+        public readonly DateTime _date;
+        private int _age;
+
+        public User()
+        {
+            _date = DateTime.Now;
+        }
+
+        public User(string login, string name, string lastName, int age)
+        {
+            Login = login;
+            Name = name;
+            LastName = lastName;
+            Age = age;
+            _date = DateTime.Now;
+
+        }
+
+        public string Login { get; } = "No Login";
+        public string Name { get; } = "No Name";
+        public string LastName { get; } = "No LastName";
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if(value >= 18)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    _age = 18;
+                }
+            }
+        }
+
+    }
+}
