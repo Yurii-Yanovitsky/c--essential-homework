5a9bcbdc975f061f93b49363c00df3040b4726ef
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-24 14:59:05 +0300

    lesson-2: add task4

---
+++ Lesson2/Task4/Invoice.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task4
+{
+    class Invoice
+    {
+        private readonly int _account;
+        private readonly string _customer;
+        private readonly string _provider;
+        private readonly double _ndcKoef;
+
+        public Invoice(int account, string customer, string provider, double ndcKoef)
+        {
+            _account = account;
+            _customer = customer;
+            _provider = provider;
+            _ndcKoef = ndcKoef;
+        }
+
+        public Invoice()
+        {
+        }
+
+        public string Article { get; set; }
+        public int Quantity { get; set; }
+        public double UnitCost { get; set; }
+
+        public void CalculatePaymenAmount(bool isNdc)
+        {
+            if (isNdc)
+            {
+                Console.WriteLine($"Payment amount: {UnitCost * Quantity + UnitCost * Quantity * _ndcKoef}$");
+            }
+            else
+            {
+                Console.WriteLine($"Payment amount: {UnitCost * Quantity}$");
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Lesson2/Task4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Invoice invoice = new Invoice(324235, "Frank", "SKODA", 0.2)
+            {
+                Article = "Skoda Octavia",
+                Quantity = 1,
+                UnitCost = 11000,
+            };
+
+            invoice.CalculatePaymenAmount(true);
+            invoice.CalculatePaymenAmount(false);
+
+            Console.ReadKey();
+        }
+    }
+}
