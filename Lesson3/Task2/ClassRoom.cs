0ecef4b39a354b6827e76ca8e79ba0072921689f
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-26 18:45:55 +0200

    lesson-3: added task2;

---
+++ Lesson3/Task2/ClassRoom.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task2
+{
+    public class ClassRoom
+    {
+        private List<Pupil> _pupils;
+
+        public ClassRoom(List<Pupil> pupils)
+        {
+            _pupils = pupils;
+        }
+
+        public void ShowInfo()
+        {
+            foreach (Pupil pupil in _pupils)
+            {
+                Console.WriteLine(pupil.Name);
+                pupil.Study();
+                pupil.Read();
+                pupil.Write();
+                pupil.Relax();
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Lesson3/Task2/Pupil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Task2
+{
+    public abstract class Pupil
+    {
+        public Pupil(string name)
+        {
+            Name = name;
+        }
+
+        public string  Name { get; }
+        public virtual void Study()
+        {
+            Console.WriteLine("I can study.");
+        }
+        public virtual void Read()
+        {
+            Console.WriteLine("I can read.");
+        }
+        public virtual void Write()
+        {
+            Console.WriteLine("I can write.");
+        }
+        public virtual void Relax()
+        {
+            Console.WriteLine("I can relax.");
+        }
+    }
+}
