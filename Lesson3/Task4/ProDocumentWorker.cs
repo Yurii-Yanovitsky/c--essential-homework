0f7650b5f6763f39fcf220640717a14429d02d13
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-27 17:10:46 +0200

    lesson-3: added task4

---
+++ Lesson3/Task4/ProDocumentWorker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task4
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        //public override void OpenDocument()
+        //{
+        //    Console.WriteLine();
+        //}
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт"); ;
+        }
+    }
+}
---
+++ Lesson3/Task4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input key (pro/exp)");
+            string key = Console.ReadLine();
+            DocumentWorker worker = CreatDocument(key);
+
+            worker.OpenDocument();
+            worker.EditDocument();
+            worker.SaveDocument();
+
+            Console.ReadKey();
+        }
+        static DocumentWorker CreatDocument(string key)
+        {
+            if (key.ToLower() == "pro")
+            {
+                return new ProDocumentWorker();
+            }
+            else if (key.ToLower() == "exp")
+            {
+                return new ExpertDocumentWorker();
+            }
+            else
+            {
+                return new DocumentWorker();
+            }
+        }
+    }
+}
