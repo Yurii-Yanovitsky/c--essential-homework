0b7e58b708114cfefbcb3b60b6a7a406699710c4
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-10-27 22:44:17 +0200

    lesson-4: added task2, again

---
+++ Lesson4/Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Editor editor = new Editor();
+            editor.ChooseDocument("MyDocument.doc");
+            editor.Open();
+            editor.Edit();
+            editor.Save();
+
+            editor.ChooseDocument(".txt");
+            editor.Creat();
+
+            Console.ReadKey();
+        }
+    }
+}
