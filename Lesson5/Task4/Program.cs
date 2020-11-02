b474591ae1ff13db94634ef964785ccc176e22d6
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-02 17:11:13 +0200

    lesson-5: added task4

---
+++ Lesson5/Task4/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store("ATB");
+
+            Article[] articles =
+            {
+                new Article("Bread", store, 15),
+                new Article("Sugar", store, 20),
+                new Article("Rice", store, 20),
+                new Article("Eggs", store, 27),
+                new Article("Potatoes", store, 8)
+            };
+
+            store.GetProducts(articles);
+
+            ShowProductInfo(store, 0);
+            ShowProductInfo(store, 3);
+            ShowProductInfo(store, 5);
+
+            string productName = Console.ReadLine();
+            ShowProductInfo(store, productName);
+
+            Console.ReadLine();
+        }
+        static void ShowProductInfo(Store store, int number)
+        {
+            string s = store[number] == null ? "Product isn't found" : store[number].ToString();
+            Console.WriteLine($"{s}\n");
+        }
+        static void ShowProductInfo(Store store, string name)
+        {
+            string s = store[name] == null ? "Product isn't found" : store[name].ToString();
+            Console.WriteLine($"{s}\n");
+        }
+    }
+}
---
+++ Lesson5/Task4/Store.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task4
+{
+    class Store
+    {
+        private Article[] _articles = new Article[0];
+
+        public Store(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+        public Article this[int index]
+        {
+            get
+            {
+                if (index < _articles.Length)
+                {
+
+                    return _articles[index];
+                }
+                else
+                {
+
+                    return null;
+                }
+            }
+        }
+        public Article this[string name]
+        {
+            get
+            {
+
+                for (int i = 0; i < _articles.Length; i++)
+                {
+                    if (_articles[i].ProductName == name)
+                    {
+
+                        return _articles[i];
+                    }
+                }
+
+                return null;
+            }
+        }
+        public void GetProducts(params Article[] articles)
+        {
+            _articles = articles;
+        }
+
+    }
+}
