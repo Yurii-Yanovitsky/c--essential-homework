432128e67cddcf4170074052a47957f6d8b02efa
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-03 23:04:41 +0200

    lesson-6: added task2

---
+++ Lesson6/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string melody = "987 876 765 654 543 432 111 8";
+            Note[] notes = MelodyParser.ParseMelody(melody);
+            Player.Play(notes);
+
+            Console.ReadLine();
+        }
+    }
+}
