36d296a735c5cb871d7e5bdef819d19e8793a67a
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-05 00:06:58 +0200

    lesson-6: added task3

---
+++ Lesson6/Task3/ExtensionClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    public static class ExtensionClass
+    {
+        public static void Sort(this int[] array)
+        {
+
+            for (int i = 0; i < array.Length; i++)
+            {
+
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Lesson6/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Task3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] ints = { 4, 2, 5, 1, 20, -8, 20, -1, 0, 10 };
+
+            ints.Sort();
+
+            foreach (var item in ints)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
