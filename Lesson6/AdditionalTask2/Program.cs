d449b2b5e53c537155479f066c37ba7140646797
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-08 22:12:50 +0200

    lesson-6: add additional task2

---
+++ Lesson6/AdditionalTask2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = @"
+this is 1 string
+this is 2 string
+this is 3 string
+this is 4 string
+this is 5 string
+this is 6 string
+this is 7 string
+this is 8 string
+this is 9 string";
+
+            var result = TextFinder.FindAllOccurances("string", text, 5);
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine($"Occurance {i + 1}: {result[i]}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lesson6/AdditionalTask2/TextFinder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AdditionalTask2
+{
+    public static class TextFinder
+    {
+        static public string[] FindAllOccurances(string subtext, string text, int n)
+        {
+            string[] strArray = text.Split(new char[1] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] result = new string[CountAllOccurances()];
+            int index = 0;
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                if (n % 2 == 0)
+                {
+                    Console.Write("The number of context lines must be odd!");
+                    break;
+                }
+
+                else if (strArray[i].Contains(subtext) && i - n / 2 >= 0 && i + n / 2 < strArray.Length)
+                {
+                    result[index] = "";
+
+                    for (int j = i - n / 2; j <= i + n / 2; j++)
+                    {
+                        result[index] += strArray[j];
+                    }
+
+                    index++;
+                }
+
+                else if (strArray[i].Contains(subtext))
+                {
+                    result[index] = strArray[i];
+                    index++;
+                }
+            }
+
+            int CountAllOccurances()
+            {
+                int count = 0;
+
+                for (int j = 0; j < strArray.Length; j++)
+                {
+                    if (strArray[j].Contains(subtext))
+                    {
+                        count++;
+                    }
+                }
+
+                return count;
+            }
+
+            return result;
+        }
+    }
+}
