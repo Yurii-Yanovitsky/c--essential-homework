475f7260f014e023db3edd6e6e72e949bccf780d
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-09 15:41:44 +0200

    lesson-7: added task2

---
+++ Lesson7/Task2/Train.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task2
+{
+    public struct Train
+    {
+        public Train(string name, string destination, int trainNumber, DateTime departuerTime)
+        {
+            Name = name;
+            Destination = destination;
+            TrainNumber = trainNumber;
+            DepartureTime = departuerTime;
+        }
+
+        public string Name { get; }
+        public string Destination { get; }
+        public int TrainNumber { get; }
+        public DateTime DepartureTime { get; }
+    }
+}
