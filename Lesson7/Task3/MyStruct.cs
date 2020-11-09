001d5da4eeff00ca2cc12cdb9b03bc5025131681
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-09 16:05:51 +0200

    lesson-7: added task3

---
+++ Lesson7/Task3/MyStruct.cs
@@ -0,0 +1,11 @@
+﻿namespace Task3
+{
+    public struct MyStruct
+    {
+        public string Change { get; set; }
+        public MyStruct(string change)
+        {
+            Change = change;
+        }
+    }
+}
