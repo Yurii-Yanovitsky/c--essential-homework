50601d899dbae5c348d9fde8ef03184009447523
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-10 15:12:48 +0200

    lesson-8: added task3

---
+++ Lesson8/Task3/Accountant.cs
@@ -0,0 +1,17 @@
+﻿namespace Task3
+{
+    public class Accountant
+    {
+        public bool AskForBonus(Post worker, int hours)
+        {
+            if ((int)worker < hours)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
