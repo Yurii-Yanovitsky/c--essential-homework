ae9cd86432b74b36af21b0a4185fc8ff4838b418
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-10 14:48:31 +0200

    lesson-8: added task2

---
+++ Lesson8/Task2/Printer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task2
+{
+    static class Printer
+    {
+        public static void Print(string str, int color)
+        {
+            ConsoleColor col = (ConsoleColor)Enum.Parse(typeof(ConsoleColor),((Color)color).ToString());
+            Console.ForegroundColor = col;
+            Console.WriteLine(str);
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+
+}
