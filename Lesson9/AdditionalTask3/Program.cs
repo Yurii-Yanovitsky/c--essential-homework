14f9c74f6924807918b8f7ade1784482f158b78e
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-11 21:28:10 +0200

    lesson-9: added additional task3

---
+++ Lesson9/AdditionalTask3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AdditionalTask3
+{
+    public delegate double BinaryExpression(double a, double b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArrayOfInt arrayOfInt = new DynamicArrayOfInt(10);
+
+            for (int i = 0; i < arrayOfInt.Size; i++)
+            {
+                arrayOfInt.Add(i);
+            }
+
+            var result = arrayOfInt.Accumulate((sum, x) => sum + x);
+            Console.WriteLine($"Result: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
