df223dc5369219f2e21cfd111c6e13ceceb62cfd
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-11 14:55:05 +0200

    lesson-9: added additional task

---
+++ Lesson9/AdditionalTask/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    public delegate double MyDelegate(int a, int b, int c);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("b: ");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.Write("c: ");
+            int c = int.Parse(Console.ReadLine());
+
+            MyDelegate myDelegate = (int x, int y, int z) => (double)(x + y + z) / 3;
+
+            var result = myDelegate(a, b, c);
+            Console.WriteLine($"Average: {result}");
+
+            Console.ReadLine();
+        }
+    }
+}
