192a2e2e0ed29619a6229115c2dcdb66590138c5
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-11 18:21:38 +0200

    lesson-9: added additional task2

---
+++ Lesson9/AdditionalTask1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask1
+{
+    public delegate bool FilterDelegate(int a);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArrayOfInt arrayOfInt = new DynamicArrayOfInt(10);
+
+            for (int i = 0; i < arrayOfInt.Size; i++)
+            {
+                arrayOfInt.Add(i);
+            }
+
+            var result1 = arrayOfInt.Filter(element => element % 2 == 0);
+            var result2 = arrayOfInt.Filter(element => element > 5);
+            var result3 = arrayOfInt.Filter(element => element % 4 == 0);
+
+            Console.Write("result1: ");
+
+            foreach (var item in result1)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.Write("\nresult2: ");
+
+            foreach (var item in result2)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.Write("\nresult3: ");
+
+            foreach (var item in result3)
+            {
+                Console.Write($" {item}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
