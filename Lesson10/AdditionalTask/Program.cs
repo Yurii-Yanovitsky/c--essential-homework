52689523d1f37cb4c8dadad5fa73c5ddcd080dca
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-14 18:34:46 +0200

    lesson-10: added additional task

---
+++ Lesson10/AdditionalTask/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result1 = MyClass<int>.FactoryMethod();
+            var result2 = MyClass<char>.FactoryMethod();
+            var result3 = MyClass<int>.FactoryMethod();
+            var result4 = MyClass<bool>.FactoryMethod();
+            var result5 = MyClass<Program>.FactoryMethod();
+
+            Console.WriteLine(result1.GetType().Name);
+            Console.WriteLine(result2.GetType().Name);
+            Console.WriteLine(result3.GetType().Name);
+            Console.WriteLine(result4.GetType().Name);
+            Console.WriteLine(result5.GetType().Name);
+
+            Console.ReadLine();
+        }
+    }
+    static class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T ();
+        }
+    }
+}
