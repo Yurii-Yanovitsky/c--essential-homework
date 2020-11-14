abbf73cb9d12167cd4f768c06bad6a3040f07f8f
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-14 18:12:31 +0200

    lesson-10: added task4

---
+++ Lesson10/Task4/ExtansionClass.cs
@@ -0,0 +1,17 @@
+﻿namespace Task4
+{
+    public static class ExtansionClass
+    {
+        public static T[] GetArray<T>(this MyList<T> list)
+        {
+            T[] newArray = new T[list.Length];
+
+            for (int i = 0; i < list.Length; i++)
+            {
+                newArray[i] = list[i];
+            }
+
+            return newArray;
+        }
+    }
+}
