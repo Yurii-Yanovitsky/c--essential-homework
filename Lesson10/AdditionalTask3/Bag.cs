b862122fc46fa703f81041faeaf1f0bb7d605ace
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-15 23:49:59 +0200

    lesson-10: added additional task3

---
+++ Lesson10/AdditionalTask3/Bag.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+
+namespace AdditionalTask3
+{
+    class Bag<T> : IEnumerable 
+    {
+        private const int _defaultSize = 4;
+        private int _sizeKoef = 2;
+        private T[] _list;
+
+        public Bag() : this(_defaultSize)
+        {
+
+        }
+
+        public Bag(int size)
+        {
+            Length = 0;
+            Size = size;
+            _list = new T[size];
+        }
+
+        public int Length { get; private set; }
+        public int Size { get; private set; }
+        public T this[int index]
+        {
+            get
+            {
+                return _list[index];
+            }
+            set
+            {
+                if (Length > 0) _list[index] = value;
+            }
+        }
+
+        public void Add(T element)
+        {
+            if (Length < Size)
+            {
+                _list[Length++] = element;
+            }
+            else
+            {
+                ResizeArray(_list);
+                Size = _list.Length;
+                this.Add(element);
+            }
+        }
+
+        private void ResizeArray(T[] source)
+        {
+            T[] newArray = new T[source.Length * _sizeKoef];
+
+            for (int i = 0; i < source.Length; i++)
+            {
+                newArray[i] = source[i];
+            }
+            _list = newArray;
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return _list.GetEnumerator();
+        }
+    }
+}
---
+++ Lesson10/AdditionalTask3/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace AdditionalTask3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bag = new Bag<Stuff>();
+            bag.Add(new CreditCard());
+            bag.Add(new Wallet());
+            bag.Add(new Passport());
+            GoShopping(bag);
+
+            foreach (var item in bag)
+            {
+                Console.WriteLine(item.GetType().Name);
+            }
+
+            Console.ReadLine();
+        }
+        static void GoShopping(Bag<Stuff> bag)
+        {
+            Stuff[] products = new Stuff[7];
+            products[0] = new Rice();
+            products[1] = new Bread();
+            products[2] = new Milk();
+            products[3] = new Cheese();
+            products[4] = new Ham();
+            products[5] = new Drugs();
+            products[6] = new Gun();
+
+            PayForProducts(bag);
+            GiveBagToTheGuyWhoPacksProducts(bag, products);
+
+        }
+        static void GiveBagToTheGuyWhoPacksProducts(Bag<Stuff> bag, Stuff[] products)
+        {
+
+            foreach (var product in products)
+            {
+                if (product is Product p)
+                {
+                    bag.Add(p);
+                }
+            }
+        }
+        static void PayForProducts(Bag<Stuff> bag)
+        {
+
+            foreach (var item in bag)
+            {
+                if (item is Wallet)
+                {
+                    Console.WriteLine("Pay products!");
+                }
+            }
+        }
+    }
+    abstract class Stuff { }
+    abstract class PersonalStuff : Stuff { }
+    class CreditCard : PersonalStuff { }
+    class Wallet : PersonalStuff { }
+    class Cigarettes : PersonalStuff { }
+    class Lighter : PersonalStuff { }
+    class Passport : PersonalStuff { }
+    abstract class IllegalObject : Stuff { }
+    class Gun : IllegalObject { }
+    class Drugs : IllegalObject { }
+    abstract class Product : Stuff { }
+    class Rice : Product { }
+    class Bread : Product { }
+    class Milk : Product { }
+    class Cheese : Product { }
+    class Ham : Product { }
+}
