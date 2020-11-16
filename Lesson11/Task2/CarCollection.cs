6b8d242fa7fee20969622c2343211af0f17fddae
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-16 15:54:26 +0200

    lesson-11: added task2

---
+++ Lesson11/Task2/CarCollection.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+
+namespace Task2
+{
+    public class CarCollection<T> : IEnumerable
+    {
+        private const int defaultSize = 10;
+        private T[] _parkingLot;
+
+        public CarCollection() : this(defaultSize)
+        {
+
+        }
+
+        public CarCollection(int initialSize)
+        {
+            Count = 0;
+            _parkingLot = new T[initialSize];
+        }
+
+        public int Count { get; private set; }
+        public int Capacity => _parkingLot.Length;
+        public void Add(T car)
+        {
+            if (Count < Capacity)
+            {
+                _parkingLot[Count++] = car;
+            }
+            else
+            {
+                Console.WriteLine("Parking lot is overloaded!");
+            }
+        }
+        public void ClearPark()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                _parkingLot[i] = default;
+            }
+            Count = default;
+        }
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < Count)
+                {
+                    return _parkingLot[index];
+                }
+                else
+                {
+                    return default;
+                }
+            }
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return _parkingLot[i];
+            }
+        }
+    }
+}
---
+++ Lesson11/Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarCollection<Car> park = new CarCollection<Car>();
+
+            Console.WriteLine($"Capacity: {park.Capacity}");
+
+            park.Add(new Car("BMW", "2005"));
+            park.Add(new Car("FORD", "2010"));
+            park.Add(new Car("SKODA", "2015"));
+            park.Add(new Car("PORSHE", "2018"));
+            park.Add(new Car("AUDI", "2020"));
+
+            foreach (var car in park)
+            {
+                Console.WriteLine(car.ToString());
+            }
+
+            Console.WriteLine($"Number of cars: {park.Count}");
+
+            Console.WriteLine($"\n{park[4]}\n");
+
+            park.ClearPark();
+            Console.WriteLine("Clearing is done!");
+            Console.WriteLine($"Number of cars: {park.Count}");
+
+            Console.ReadLine();
+        }
+    }
+}
