fcf63782f6b1e8d95b4be774dc6e96d896780b4a
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-18 17:37:23 +0200

    lesson-11: added task4

---
+++ Lesson11/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList notGenericList = new ArrayList(10);
+
+            notGenericList.Add("Hello!");
+            notGenericList.Add('A');
+            notGenericList.Add(5);
+            notGenericList.Add(24.5);
+            notGenericList.Add(true);
+
+            foreach (var item in notGenericList)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
