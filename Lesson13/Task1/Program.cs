0718270a4780ec07352d9e0d17d026c51cc2f0d5
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-26 20:03:25 +0200

    lesson-13: added task1

---
+++ Lesson13/Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.SetWindowSize(80, 40);
+
+            for (int i = 0; i < 30; i++)
+            {
+                new Thread(new Matrix(i * 2).Move).Start();
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
