6c1a1af75d48dfccf56119451dd702d5a4edbd12
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-27 00:33:43 +0200

    lesson-14: added additional task

---
+++ Lesson14/AdditionalTask/ExtensionClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+
+namespace AdditionalTask
+{
+    static class ExtensionClass
+    {
+        public static IEnumerable GetEven(this int[] numbers, Func<int, bool> action)
+        {
+            foreach (var n in numbers)
+            {
+                if (action(n))
+                {
+                    yield return n;
+                }
+            }
+        }
+    }
+}
