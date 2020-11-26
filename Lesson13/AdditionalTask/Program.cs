0457267ce25a99633683ce9ab158fd8c7066e30f
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-26 20:02:47 +0200

    lesson-13: added additional taks

---
+++ Lesson13/AdditionalTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static int count = 0;
+        static void MyMethod(object str)
+        {
+            Console.WriteLine($"Call MyMethod on thread {count}");
+            Thread.Sleep(1500);
+            Thread thread = new Thread(MyMethod);
+            count++;
+            thread.Start();
+        }
+        static void Main()
+        {
+            Thread thread = new Thread(MyMethod);
+            thread.Start();
+        }
+    }
+}
