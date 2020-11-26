483fca299bd546bccc9cd91ee48c21d6bfcaba4e
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-27 01:10:18 +0200

    lesson-14: added task3

---
+++ Lesson14/Task3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string, string> dictionary = new MyDictionary<string, string>();
+
+            dictionary.Add("key1", "Monday");
+            dictionary.Add("key2", "Tuesday");
+            dictionary.Add("key3", "Wednesday");
+            dictionary.Add("key5", "Friday");
+
+            foreach (var dic in dictionary)
+            {
+                Console.WriteLine(dic);
+            }
+            
+            IEnumerable<Entry<string, string>> dictionary1 = dictionary;
+            Console.WriteLine();
+
+            dictionary["key1"] = "Monday";
+            dictionary["key4"] = "Thursday";
+
+            foreach (var dic in dictionary1)
+            {
+                Console.WriteLine(dic);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
