157830e26d18fd21074d9bfdabddb3ce20fca329
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-27 01:10:45 +0200

    lesson-14: added task4

---
+++ Lesson14/Task4/ExtensionClass.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Task4
+{
+    public static class ExtensionClass
+    {
+        public static T[] GetArray<T>(this IEnumerable<T> list)
+        {
+            int index = 0;
+
+            T[] newArray = new T[list.Count()];
+
+            foreach (var item in list)
+            {
+                newArray[index] = item;
+                index++;
+            }
+
+            return newArray;
+        }
+    }
+}
