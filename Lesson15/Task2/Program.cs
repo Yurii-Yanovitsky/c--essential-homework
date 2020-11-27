f7008520b01fdab167636ff34499471e767a7c3e
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-28 01:17:17 +0200

    lesson-15: added task2

---
+++ Lesson15/Task2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var workers = CreatListOfWorkers();
+
+            Console.WriteLine("СПИСОК СОТРУДНИКОВ");
+            foreach (var worker in workers)
+            {
+                Console.WriteLine(worker);
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Введите необходимый стаж работы: ");
+            int experience = Convert.ToInt32(Console.ReadLine());
+            workers = workers.Where((x) => x.Experience > experience).ToArray();
+
+            foreach (var worker in workers)
+            {
+                Console.WriteLine($"Фамилия: {worker.Name}, Стаж:{worker.Experience}");
+            }
+
+
+            Console.ReadLine();
+        }
+
+        static Worker[] CreatListOfWorkers()
+        {
+            Worker[] workers = new Worker[5];
+
+            string secondName;
+            string pos;
+            int year;
+
+            for (int i = 0; i < workers.Length; i++)
+            {
+                Console.WriteLine("Введите фамилию: ");
+                secondName = Console.ReadLine();
+
+                Console.WriteLine("Введите должность: ");
+                pos = Console.ReadLine();
+
+                while(true)
+                {
+                    try
+                    {
+                        Console.WriteLine("Введите год приема на работу: ");
+                        year = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.WriteLine("Используйте числа!");
+                    }
+                }
+
+                workers[i] = new Worker(secondName, pos, year);
+
+                Console.Clear();
+            }
+
+            workers = workers.OrderBy((x) => x.Name).ToArray();
+
+            return workers;
+        }
+    }
+}
---
+++ Lesson15/Task2/Worker.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Task2
+{
+    struct Worker
+    {
+        private int _year;
+        public Worker(string name, string pos, int year)
+        {
+            _year = DateTime.Now.Year;
+            Position = pos;
+            Name = name;
+            Year = year;
+        }
+
+        public string Name { get; }
+        public string Position { get; }
+        public int Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (value <= DateTime.Now.Year && DateTime.Now.Year - value <= 40)
+                {
+                    _year = value;
+                }
+                else
+                {
+                    Console.WriteLine("Некорректно задан год! Введите еще раз:");
+                    Year = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+        public int Experience
+        {
+            get
+            {
+                return DateTime.Now.Year - Year;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Фамилия: {Name}, Должность: {Position}, Год: {Year}";
+        }
+    }
+}
