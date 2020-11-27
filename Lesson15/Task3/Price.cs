3af23d7b9053e7d00d48916b4507f89d2caf923b
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-28 01:19:10 +0200

    lesson-15: added task3

---
+++ Lesson15/Task3/Price.cs
@@ -0,0 +1,21 @@
+﻿namespace Task3
+{
+    struct Price
+    {
+        public string ProductName { get; private set; }
+        public string Shop { get; private set; }
+        public int Cost { get; set; }
+
+        public Price(string productName, string shop, int cost)
+        {
+            ProductName = productName;
+            Cost = cost;
+            Shop = shop;
+        }
+
+        public override string ToString()
+        {
+            return $"Товар: {ProductName}, Магазин: {Shop}, Цена: {Cost}";
+        }
+    }
+}
---
+++ Lesson15/Task3/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Linq;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = CreateProduct();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Введите имя искомого магазина");
+                    string shopName = Console.ReadLine();
+
+                    products = products.Where((x) => x.Shop == shopName).ToArray();
+
+                    if (products.Length != 0)
+                    {
+                        foreach (var product in products)
+                        {
+                            Console.WriteLine(product);
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        throw new Exception("Нет такого магазина!");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    Console.WriteLine("Введите \"EXIT\", чтобы закончить поиск.");
+                    if("EXIT" == Console.ReadLine().ToUpper())
+                    {
+                        break;
+                    }
+                }
+            }
+        }
+
+        static Price[] CreateProduct()
+        {
+            Price[] products = new Price[2];
+
+            string productName;
+            string shop;
+            int cost;
+
+            for (int i = 0; i < products.Length; i++)
+            {
+                Console.WriteLine("Введите название товара: ");
+                productName = Console.ReadLine();
+
+                Console.WriteLine("Введите название магазина: ");
+                shop = Console.ReadLine();
+
+                while (true)
+                    try
+                    {
+                        Console.WriteLine("Введите цену на товар: ");
+                        cost = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+
+                products[i] = new Price(productName, shop, cost);
+
+                Console.Clear();
+            }
+            products = products.OrderBy((x) => x.Shop).ToArray();
+
+            return products;
+        }
+    }
+}
