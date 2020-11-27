e491eb1dca0923fac38f62a932b47841b0f79360
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-27 16:04:17 +0200

    lesson-17: added additional task

---
+++ Lesson17/AdditionalTask/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+
+            double a = 100;
+            double b = 35;
+
+            Console.WriteLine($"{a} + {b} = {calculator.Add(a, b)}");
+            Console.WriteLine($"{a} - {b} = {calculator.Subtract(a, b)}");
+            Console.WriteLine($"{a} * {b} = {calculator.Mul(a, b)}");
+            Console.WriteLine($"{a} / {b} = {calculator.Divide(a, b)}");
+
+            Console.ReadLine();
+        }
+    }
+}
