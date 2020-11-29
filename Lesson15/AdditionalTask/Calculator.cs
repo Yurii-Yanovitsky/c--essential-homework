578df2d3d80e52e5abad0951b8952d83bf9a4d3a
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-11-29 16:32:27 +0200

    lesson-15: added additional task

---
+++ Lesson15/AdditionalTask/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace AdditionalTask
+{
+    class Calculator
+    {
+        public double Add(double a, double b) => a + b;
+        public double Subtract(double a, double b) => a - b;
+        public double Mul(double a, double b) => a * b;
+        public double? Divide(double a, double b)
+        {
+            try
+            {
+                if (b != 0)
+                {
+                    return a / b;
+                }
+                else
+                {
+                    throw new Exception("На нуль делить нельзя!");
+                }
+     
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
