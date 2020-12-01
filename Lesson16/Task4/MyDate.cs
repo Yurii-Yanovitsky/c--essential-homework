6d1b8c774edca58f9729c4ce21fe7a7b9585ea65
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-01 16:56:03 +0200

    lesson-16

---
+++ Lesson16/Task4/MyDate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Task4
+{
+    class MyDate
+    {
+        public MyDate(DateTime date)
+        {
+            Date = date;
+        }
+
+        public DateTime Date { get; }
+
+
+        public static int operator -(MyDate d1, MyDate d2)
+        {
+            TimeSpan diff = d1.Date - d2.Date;
+
+            return diff.Days;
+        }
+
+        public static MyDate operator +(MyDate d1, int days)
+        {
+            MyDate myDate = new MyDate(d1.Date.AddDays(days));
+
+            return myDate;
+        }
+
+        public override string ToString()
+        {
+            return $"{Date.ToShortDateString()}";
+        }
+    }
+}
