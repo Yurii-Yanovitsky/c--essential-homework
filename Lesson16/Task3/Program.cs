2eee067358689b683a7c347b94b46585fcdc034a
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-01 12:20:16 +0200

    lesson-16: added task3

---
+++ Lesson16/Task3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Clone()");
+            House originHouse = new House("New York", new Street("Wall Street"));
+            House cloneHouse = originHouse.Clone();
+
+            Console.WriteLine("Before changing the address:");
+            Console.WriteLine($"original: {originHouse}");
+            Console.WriteLine($"clone: {cloneHouse}");
+
+            cloneHouse.Street.Name = "Baker street";
+            cloneHouse.City = "London";
+
+            Console.WriteLine("After changing the address:");
+            Console.WriteLine($"original: {originHouse}");
+            Console.WriteLine($"clone: {cloneHouse}");
+
+            Console.WriteLine();
+
+            Console.WriteLine("DeepClone()");
+            House originHouse1 = new House("New York", new Street("Wall Street"));
+            House cloneHouse1 = originHouse1.DeepClone();
+
+            Console.WriteLine("Before changing the address:");
+            Console.WriteLine($"original: {originHouse1}");
+            Console.WriteLine($"clone: {cloneHouse1}");
+
+            cloneHouse1.Street.Name = "Baker street";
+            cloneHouse1.City = "London";
+
+            Console.WriteLine("After changing the address:");
+            Console.WriteLine($"original: {originHouse1}");
+            Console.WriteLine($"clone: {cloneHouse1}");
+
+            Console.ReadLine();
+        }
+    }
+}
