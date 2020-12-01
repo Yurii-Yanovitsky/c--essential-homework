d13ede3241563ae607fb1d1de3203ab4c39dd1a5
Yurii Yanovitsky <yanovitsky1998@gmail.com>
2020-12-01 22:29:05 +0200

    lesson-18: added task2

---
+++ Lesson18/Task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Task2_Library;
+
+namespace Task2
+{
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass myClass = new MyClass();
+            myClass.Method1();
+              
+            DerivedClass derivedClass = new DerivedClass();
+            derivedClass.CallMethod2();
+
+            Console.ReadLine();
+        }
+    }
+    public class DerivedClass : MyClass
+    {
+        public void CallMethod2()
+        {
+            Method2();
+        }
+    }
+
+}
